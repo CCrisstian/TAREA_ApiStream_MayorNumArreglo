f2a4086578977071c10e316ccdf79a0c29657275
CCrisstian <crisstiann.c@gmail.com>
2024-02-26 18:53:12 -0300

    Tarea Terminada

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 import java.util.Optional;
 import java.util.Random;
+import java.util.function.Function;
 
 public class Main {
     public static void main(String[] args) {
@@ -20,6 +21,15 @@ public class Main {
         Optional<Integer> maximo = Arrays.stream(arr_numeros)   /*Convertir 'Array' en 'Stream'*/
                 .reduce(Integer::max);  /*Usar 'reduce' para hallar el mayor valor*/
         System.out.println("\nMayor número del arreglo: " + maximo.get());
+
+        System.out.println("\n===============Respuesta Correcta implementando una función LAMBDA propia===============\n");
+        Function<Integer[], Integer> max = arr -> Arrays.stream(arr).reduce(0, (a, b) -> a > b ? a: b);
+        /*Function<Entrada, Salida>*/
+        /*reduce(Valor inicial, (a, b) -> a > b ? Asignar a 'a' el valor de 'b');*/
+
+        Integer resultado = max.apply(arr_numeros);
+        System.out.println("Mayor número del arreglo: " + resultado);
+
 
     }
 }
